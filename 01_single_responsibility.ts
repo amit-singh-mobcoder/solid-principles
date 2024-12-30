823cc241be618843ff5f72a46c6fd52664853f8c
amit-singh-mobcoder <amit.s@mobcoder.com>
2024-12-30 09:55:24 +0530

    another example added for single responsibility principle

--- 01_single_responsibility.ts
+++ 01_single_responsibility.ts
@@ -25,6 +25,38 @@ class FileSaver {
 
 
 
+// -------------- Another Example ---------------------------
+// BAD Example: Suppose we have a class handling both user data and email sending.
+
+class UserServices {
+    createUser(username: string, email: string) {
+        // Save user to database
+        console.log(`User ${username} created.`);
+
+        // Send welcome email
+        console.log(`Welcome email sent to ${email}.`);
+    }
+}
+
+// Improved Example: Split the responsibilities into separate classes:
+class UserService {
+    createUser(username: string) {
+        console.log(`User ${username} created.`);
+    }
+}
+
+class EmailService {
+    sendWelcomeEmail(email: string) {
+        console.log(`Welcome email sent to ${email}.`);
+    }
+}
+
+// Usage
+const userService = new UserService();
+const emailService = new EmailService();
+
+userService.createUser("JohnDoe");
+emailService.sendWelcomeEmail("john@example.com");
 
 
 
